aca9688eede91c3440f79684412304a8b4aa65d8
Arslan Rana <rajputarslan693@gmail.com>
2024-02-25 14:21:22 +0500

    client_id, secret

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,19 @@
-
+import Feed from "@components/Feed";
 
 export default function Home() {
   return (
-     <>
-     <div>Homes</div>
-     </>
+     <section className="w-full flex-center flex-col ">
+       
+       <h1 className="head_text text-center"> Discover & Share
+       <br className="max-md:hidden"/> <br />
+       <span className="orange_gradient text-center">AI-Powerd-Prompts</span>
+       </h1>
+       <p className="desc text-center">
+       Discover the best AI prompts for ChatGPT, Midjourney, Dall-E, 
+       and more. Save them and create
+        collections with your team
+       </p>
+       <Feed/>
+     </section>
   );
 }
